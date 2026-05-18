1099f12e89acbe7ecc1b76217562db2738321ced
Berilkdd <brlkrdnz@hotmail.com>
2026-05-18 13:59:15 +0100

    Add and check new packages

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*c11u*k&)kd+(wncgs31nh!z4)$3bqpumc@4m9he=ca-ib43_='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
